62c4d175079c30fed79a563fc52f4a15dc01b75b
bred <bredzhang@qq.com>
2020-04-21 19:44:09 +0800

    add  move

--- src/list.h
+++ src/list.h
@@ -97,6 +97,11 @@ static inline void __list_del(struct list_head * prev, struct list_head * next)
 	prev->next = next;
 }
 
+static inline void __list_del_entry(struct list_head *entry)
+{
+    __list_del(entry->prev, entry->next);
+}
+
 /**
  * list_del - deletes entry from list.
  * @entry: the element to delete from the list.
@@ -110,6 +115,16 @@ static inline void list_del(struct list_head *entry)
 	entry->prev = LIST_POISON2;
 }
 
+/**
+ * list_move - delete from one list and add as another's head
+ * @list: the entry to move
+ * @head: the head that will precede our entry
+ */
+static inline void list_move(struct list_head *list, struct list_head *head)
+{
+    __list_del_entry(list);
+    list_add(list, head);
+}
 
 /**
  * list_is_last - tests whether @list is the last entry in list @head
