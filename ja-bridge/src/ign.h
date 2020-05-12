5c61263794c1838289cf96821abd31e832267f5f
bred <bredzhang@qq.com>
2020-05-12 11:35:23 +0800

    update

--- ja-bridge/src/ign.h
+++ ja-bridge/src/ign.h
@@ -17,7 +17,7 @@ atomic_int g_msg_id = 0;
 
 #define HOST "ssl://aa85fsnk5qn58-ats.iot.ap-southeast-1.amazonaws.com:8883"
 #define SUBSCRIBE_CLIENT_ID "JasonSubscribeID"
-#define CA_PATH "../..//igkey/"
+#define CA_PATH "../../igkey/"
 #define TRUST_STORE "../../igkey/ca.pem"
 #define PRIVATE_KEY "../../igkey/key.pem"
 #define KEY_STORE "../../igkey/cert.pem"
